50590cb4cd4d138e070b24357de7e1e76e208b2c
vlaskz <dragoonbr@gmail.com>
2019-09-07 13:43:43 -0300

    #removed: ARRAY_SIZE, inserted fixed length, with the number of fields

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <math.h>
 #include <stdio.h>
+
 #define LENGTH 6
 
 
@@ -13,15 +14,23 @@ void getData();
 
 void iterate();
 
-float a = 0, b = 0, x = 0, fa = 0, fb = 0, fx = 0, tol=0;
+float a = 0, b = 0, x = 0, fa = 0, fb = 0, fx = 0, tol = 0;
 
 float n_iter = 0;
+
 void printMatrix(float[]);
+
+void calculate();
 
 int main() {
     getData();
-    float matrix[(int)n_iter][7];
-    for (int i = 0; i < (int)n_iter; i++) {
+    calculate();
+    return 0;
+}
+
+void calculate() {
+    float matrix[(int) n_iter][7];
+    for (int i = 0; i < (int) n_iter; i++) {
         iterate();
         matrix[i][0] = a;
         matrix[i][1] = b;
@@ -30,29 +39,28 @@ int main() {
         matrix[i][4] = fb;
         matrix[i][5] = fx;
 
-
+        printf("%d>", i);
         printMatrix(matrix[i]);
         printf("\n");
 
     }
-    return 0;
 }
 
 void printMatrix(float arr[]) {
-    for(int i=0;i<LENGTH;i++){
-        printf("\t %f",arr[i]);
+    for (int i = 0; i < LENGTH; i++) {
+        printf("\t %f", arr[i]);
     }
 }
 
 void iterate() {
-    x = (a+b)/2;
+    x = (a + b) / 2;
     fa = function(a);
     fb = function(b);
     fx = function(x);
-    if(signal(fa)==signal(fx)){
-        a=x;
-    }else{
-        b=x;
+    if (signal(fa) == signal(fx)) {
+        a = x;
+    } else {
+        b = x;
     }
 
 }
@@ -69,7 +77,7 @@ void getData() {
     printf("Pronto! Vamos revisar: O intervalo é [%f , %f] com tolerância de %f", a, b, tol);
     n_iter = getIter(a, b, tol);
     printf("\nO número de iterações neste caso é de %i\n", (int) n_iter);
-    printf("         A\t         B\t         X\t      f(A)\t      f(B)\t      f(X)\n");
+    printf("IT       A\t         B\t         X\t      f(A)\t      f(B)\t      f(X)\n");
 }
 
 float getIter(float l_inf, float l_sup, float tol) {
