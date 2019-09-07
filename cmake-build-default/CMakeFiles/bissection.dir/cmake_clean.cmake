dbab9fd1c4b30d7026d9b802991dd48fc54d6924
root <root@localhost.localdomain>
2019-09-07 12:02:15 -0300

    Initial commit

---
+++ cmake-build-default/CMakeFiles/bissection.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/bissection.dir/main.c.o"
+  "bissection.pdb"
+  "bissection"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/bissection.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
